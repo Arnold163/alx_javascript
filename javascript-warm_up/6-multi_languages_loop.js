f1f81ae4532ce6f6d7c89dd1debf23182be98249
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-01-30 11:18:50 +0200

    loop

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+//lets try these on the same line shall we
+
+//array of strings 
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; 1 < languages.length; i++) {
+    console.log(languages[i]);
+}
