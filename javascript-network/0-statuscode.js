d80b2d0d605240fcc83f3fb7cfb1018d521904f6
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-02-07 19:49:44 +0200

    update

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -2,7 +2,7 @@
 
 const https = require('https');
 
-if (Process.argv.length !== 3) {
+if (process.argv.length !== 3) {
     console.log('usage: node 0-statuscode.js <URL>');
     process.exit(1);
 }
@@ -10,7 +10,7 @@ if (Process.argv.length !== 3) {
 const url = process.argv[2];
 
 https.get(url, (res) =>  {
-    console.log('code: ${res.statusCode}');
+    console.log(`code: ${res.statusCode}`);
 }).on('error', (err) => {
-    console.error('Error: ${err.message}');
+    console.error(`Error: ${err.message}`);
 });
