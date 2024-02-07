48eb6eb3494a88cbad51bb3a2534452cb7ce5a94
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-02-07 19:43:22 +0200

    status co

---
+++ javascript-network/0-statuscode.js
@@ -0,0 +1,16 @@
+//status code
+
+const https = require('https');
+
+if (Process.argv.length !== 3) {
+    console.log('usage: node 0-statuscode.js <URL>');
+    process.exit(1);
+}
+
+const url = process.argv[2];
+
+https.get(url, (res) =>  {
+    console.log('code: ${res.statusCode}');
+}).on('error', (err) => {
+    console.error('Error: ${err.message}');
+});
