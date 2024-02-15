8e260051c58d853fb34188b5f2acee3c95649b3d
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-02-15 20:54:02 +0200

    updta

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,27 +1,24 @@
-//lori ipsim
+//lori ipsim request
 const fs = require('fs');
-const https = require('https');
+const request = require('request');
 
 function saveWebpageContent(url, filePath) {
-    https.get(url, (response) => {
-        let data = '';
-
-        response.on('data', (chunk) => {
-            data += chunk;
+    request(url, (error, response, body) => {
+        if (error) {
+            console.error('Error fetching webpage content:', error);
+            process.exit(1);
+        }
+        if (response.statusCode !== 200) {
+            console.error('Unexpected status code:', response.statusCode);
+            process.exit(1);
+        }
+        fs.writeFile(filePath, body, 'utf8', (err) => {
+            if (err) {
+                console.error('Error writing to file:', err);
+                process.exit(1);
+            }
+            console.log(`Webpage content saved to ${filePath} successfully.`);
         });
-
-        response.on('end', () => {
-            fs.writeFile(filePath, data, 'utf8', (err) => {
-                if (err) {
-                    console.error('Error writing to file:', err);
-                    process.exit(1);
-                }
-                console.log(`Webpage content saved to ${filePath} successfully.`);
-            });
-        });
-    }).on('error', (err) => {
-        console.error('Error fetching webpage content:', err);
-        process.exit(1);
     });
 }
 
