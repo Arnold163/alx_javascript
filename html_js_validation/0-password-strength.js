f81974219fde971957ce279a41831fbdbace3e5a
Arnold163 <148977610+Arnold163@users.noreply.github.com>
2024-02-28 22:57:00 +0200

    update

--- html_js_validation/0-password-strength.js
+++ html_js_validation/0-password-strength.js
@@ -1,36 +1,38 @@
-//pass validation
-//pass validation function
-function validatePassword() {
-    const passwordInput = document.getElementById("password").value;
-    const errorElement = document.getElementById("error");
+document.addEventListener("DOMContentLoaded", function() {
+    //pass validation
+    //pass validation function
+    function validatePassword() {
+        const passwordInput = document.getElementById("password").value;
+        const errorElement = document.getElementById("error");
 
-    //expressions to check pass strength criteria
-    const minLength = 8;
-    const uppercaseRgex = /[A-Z]/;
-    const lowercaseRegex = /[a-z]/;
-    const digitRegex = /[0-9]/;
-    const specialCharRegex = /[!@#$%&*]/;
+     //expressions to check pass strength criteria
+        const minLength = 8;
+        const uppercaseRegex = /[A-Z]/;
+        const lowercaseRegex = /[a-z]/;
+        const digitRegex = /[0-9]/;
+        const specialCharRegex = /[!@#$%&*]/;
 
     //check if pass meets all requirements 
-    if (
-        passwordInput.length >= minLength &&
-        uppercaseRgex.test(passwordInput) &&
-        lowercaseRegex.test(passwordInput) &&
-        digitRegex.test(passwordInput) &&
-        specialCharRegex.test(passwordInput)
-    ){
-        //pass meets requirements, allow form
-        return true;
-    }else {
-        //not meet criteria, display error message
-        errorElement.textContent = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one numeric digit, and one special character (!@#$%^&*)."
-        return false;
+     if (
+            passwordInput.length >= minLength &&
+            uppercaseRegex.test(passwordInput) &&
+            lowercaseRegex.test(passwordInput) &&
+            digitRegex.test(passwordInput) &&
+            specialCharRegex.test(passwordInput)
+     ){
+            //pass meets requirements, allow form
+         return true;
+        }else {
+         //not meet criteria, display error message
+         errorElement.textContent = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one numeric digit, and one special character (!@#$%^&*)."
+         return false;
+        }
     }
-}
 
-//event listener
-document.getElementById("passwordForm").addEventListener("submit", function(event) {
-    if (!validatePassword()) {
-        event.preventDefault();
-    }
-}) 
+    //event listener
+    document.getElementById("passwordForm").addEventListener("submit", function(event) {
+        if (!validatePassword()) {
+         event.preventDefault();
+        }
+    });
+}); 
